b7b7820ba34d889abe62bd178fa3f33a1ca2979d
kynnyhsap <tobirawork@gmail.com>
2021-12-29 16:49:35 +0200

    feat: add tailwindcss

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      NEAR Crowd
+        <h1 className="text-3xl font-bold">
+            NEAR Crowd
+        </h1>
     </div>
   );
 }
