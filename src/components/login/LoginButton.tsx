2a5139f0c097d8a5980beb72e7856b66749e6ae8
Shockedshodan <46688871+Shockedshodan@users.noreply.github.com>
2022-04-30 19:20:58 +0100

    removed redundant logic, removed logo, removed margin

--- src/components/login/LoginButton.tsx
+++ src/components/login/LoginButton.tsx
@@ -2,7 +2,6 @@ import { useNearWallet } from 'react-near';
 import { useTranslation } from 'react-i18next';
 import {useState, useEffect} from 'react';
 import styled from 'styled-components';
-import logo from '../../assets/images/near-logo.png';
 
 import { nearEnv, fakeAuth } from '../../config/near-env';
 
@@ -16,24 +15,12 @@ const LoginButtonContainer = styled.div`
     height: 40px;
     display: flex;
     justify-content: space-evenly;
-    margin: 0 auto;
 `
 
 const { REACT_APP_NEAR_ENV } = process.env;
 const contractId  = REACT_APP_NEAR_ENV === 'testnet' ? 'usdn.testnet' : 'usn'
 
 function LoginButton() {
-    const [isDesktop, setDesktop] = useState(window.innerWidth > 1000);
-
-  const updateMedia = () => {
-    setDesktop(window.innerWidth > 1450);
-  };
-
-  useEffect(() => {
-    window.addEventListener("resize", updateMedia);
-    return () => window.removeEventListener("resize", updateMedia);
-  });
-
     const wallet = useNearWallet()!;
     const { t } = useTranslation();
 
@@ -61,7 +48,6 @@ function LoginButton() {
         >
             <div className="flex items-center space-x-3">
                 <span>{t('buttons.login')}</span>
-                {isDesktop ? ( <img src={logo} alt="near-icon" className="w-6" />) : null}
             </div>
         </LoginButtonContainer>
     );
