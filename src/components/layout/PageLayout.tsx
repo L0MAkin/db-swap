3d6b67db99ff7f12aae496bf26ffc680c8a40fab
ignatKorokov <ignat.korokov@gmail.com>
2022-06-01 15:53:33 +0300

    fix: fix layout

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -5,10 +5,14 @@ import { Footer } from '../main/components/Footer';
 
 const Wrapper = styled.div`
     width: 100%;
-    height: 100vh;
+    height: 100%;
     background-color: #FEFDEE;
     background-image: url(${bg});
     background-size: cover;
+
+    main {
+        margin-bottom: 30px;
+    }
 
     @media (max-width:1024px) {
         background-size: contain;   
@@ -17,6 +21,8 @@ const Wrapper = styled.div`
     @media (max-width:768px) {
        main {
            margin-top: 55px;
+           padding-bottom: 30px;
+           margin-bottom: 0;
        } 
     }
 `
@@ -26,7 +32,7 @@ const PageLayout: FC = ({ children }) => {
         <Wrapper>
             <div className="flex flex-col h-full">
 
-                <main className="flex-1 h-max container p-2 mx-auto" style={{marginBottom: 30}}>
+                <main className="flex-1 h-max container p-2 mx-auto" >
                     {children}
                 </main>
                 <Footer />
